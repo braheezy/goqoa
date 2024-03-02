206454ec2692d257e2761c4b72175d538e95532b
braheezy <michael.c.braha@gmail.com>
2024-03-02 10:29:57 -0800

    Improve and fix spec test script

--- check_spec.sh
+++ check_spec.sh
@@ -9,17 +9,17 @@ num_songs=10
 temp_dir=$(mktemp -d)
 
 if ! command -v goqoa &>/dev/null; then
-    echo "goqoa not found! Try make install"
+    echo "goqoa not found! Try make install" >&2
+    exit 1
 fi
 
-size_compare() {
-    size1=$(stat -c %s "$1")
-    size2=$(stat -c %s "$2")
-
-    if [ "$size1" != "$size2" ]; then
-        echo "Sizes do not match!"
-        echo "$1: $size1"
-        echo "$2: $size2"
+compare() {
+    # Large files start to differ in bytes, but they sound the same and have the same
+    # exact size. So we compare only the first 20000 bytes.
+    if ! cmp -n 20000 "$1" "$2"; then
+        echo "Files do not match!" >&2
+        echo "$1" >&2
+        echo "$2" >&2
         exit 1
     fi
 }
@@ -27,7 +27,7 @@ size_compare() {
 if [ ! -f $spec_zip ]; then
     echo "Downloading $spec_zip..."
     if command -V http &>/dev/null; then
-        http -d https://qoaformat.org/samples/qoa_test_samples_2023_02_18.zip
+        http --download --body https://qoaformat.org/samples/qoa_test_samples_2023_02_18.zip
     else
         curl -s -O https://qoaformat.org/samples/qoa_test_samples_2023_02_18.zip
     fi
@@ -37,18 +37,17 @@ fi
 selected_songs=$(unzip -Z1 "$spec_zip" '*.wav' -x '*.qoa.wav' | shuf -n "$num_songs")
 
 for song in $selected_songs; do
-    echo "Checking $song..."
+    echo -n "Checking $song..."
     song_filename=$(basename "$song")
     song_name="${song_filename%.*}"
 
     unzip -j -qq $spec_zip "*$song_name*" -d "$temp_dir"
 
-    goqoa convert "$temp_dir/$song_name.wav" "$temp_dir/my-$song_name.qoa"
-    size_compare "$temp_dir/$song_name.qoa" "$temp_dir/my-$song_name.qoa"
+    goqoa convert -q "$temp_dir/$song_name.wav" "$temp_dir/my-$song_name.qoa"
+    compare "$temp_dir/$song_name.qoa" "$temp_dir/my-$song_name.qoa"
 
-    goqoa -h
-    goqoa -q convert "$temp_dir/my-$song_name.qoa" "$temp_dir/my-$song_name.qoa.wav"
-    size_compare "$temp_dir/$song_name.qoa.wav" "$temp_dir/my-$song_name.qoa.wav"
+    goqoa convert -q "$temp_dir/my-$song_name.qoa" "$temp_dir/my-$song_name.qoa.wav"
+    compare "$temp_dir/$song_name.qoa.wav" "$temp_dir/my-$song_name.qoa.wav"
 
     echo "OK"
 done
